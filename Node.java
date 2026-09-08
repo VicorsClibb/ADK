cece3076eff23c2623982f0e8f7d0df14ff11438
Victor <victorhl@kth.se>
2026-09-08 18:41:01 +0200

    fields and constructor setup

---
+++ Node.java
@@ -0,0 +1,20 @@
+public class Node{
+
+    private final int value;
+    private final Node left, right;
+    //final för att upprätthålla beständighet (immutability)
+
+    //konstruktor
+    Node(int value, Node left, Node right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void set(Node a, int i, int value){
+
+        
+    }
+
+
+}
